3d9f5f8306d4986926f597ec738888c8d37c0fc9
Andre Castelo <andrecastelo@gmail.com>
2020-02-16 17:42:35 -0300

    add discovery

--- src/pages/Discovery.js
+++ src/pages/Discovery.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Header } from '../components';
+import { discover } from '../services/api';
 
 export const Discovery = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      const movies = await discover();
+      setMovies(movies);
+    };
+
+    getMovies();
+  }, []);
+
+  console.log({ movies });
+
   return <Header></Header>;
 };
--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,27 @@
-export const get = (url, body) => {
-  //
+import config from '../config';
+import { stringify } from 'query-string';
+
+const urlBuilder = (resource, queryData = {}) => {
+  const querystring = stringify({
+    api_key: config.apiKey,
+    ...queryData
+  });
+  const base = 'https://api.themoviedb.org/3';
+
+  return `${base}/${resource}?${querystring}`;
+};
+
+export const get = async (url, query = {}) => {
+  const response = await fetch(urlBuilder(url, query), {
+    method: 'get',
+    cors: 'none'
+  });
+
+  return await response.json();
+};
+
+export const discover = async () => {
+  const data = await get('discover/movie');
+
+  return data.results;
 };
