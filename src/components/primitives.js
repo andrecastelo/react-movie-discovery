d47798829b28bffcb1b5d1a4a3b885be0f53f2d8
Andre Castelo <andrecastelo@gmail.com>
2020-02-23 23:45:31 -0300

    fix movie detail page

--- src/components/primitives.js
+++ src/components/primitives.js
@@ -3,6 +3,6 @@ import styled from '@emotion/styled';
 export const Container = styled.div`
   width: 100%;
   box-sizing: border-box;
-  max-width: 1200px;
+  max-width: ${({ maxWidth }) => (maxWidth ? `${maxWidth}px` : '1200px')};
   margin: 0 auto;
 `;
--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,14 +1,14 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/core';
+import { jsx, css } from '@emotion/core';
 import styled from '@emotion/styled';
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import { Header, Container, SectionTitle, Rating } from '../components';
 import { getMovie, apiImage } from '../services/api';
 
 const DetailSection = styled.div`
   display: grid;
-  grid-template-columns: repeat(6, 1fr);
+  grid-template-columns: repeat(3, 1fr);
   grid-gap: 32px;
   margin-top: 32px;
   padding-bottom: 32px;
@@ -19,19 +19,19 @@ const DetailSection = styled.div`
   }
 
   div {
-    grid-column: 2 / 7;
+    grid-column: 2 / 4;
   }
 `;
 
-// const StatsSection = styled.div`
-//   display: flex;
-//   flex-direction: row;
-//   justify-content: space-between;
-//   align-items: center;
-// `;
+const StatsSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
 
 export const MovieDetail = () => {
   const { id } = useParams();
+  const history = useHistory();
   const [movie, setMovie] = useState(undefined);
 
   useEffect(() => {
@@ -50,29 +50,48 @@ export const MovieDetail = () => {
   return (
     <React.Fragment>
       <Header />
-      <Container>
+      <Container maxWidth={800} css={{ paddingBottom: 128 }}>
+        <button
+          onClick={history.goBack}
+          css={css`
+            margin-top: 32px;
+            background: transparent;
+            padding: 16px;
+            cursor: pointer;
+            border: 0;
+            font-weight: bold;
+          `}
+        >
+          &lt; Home
+        </button>
         {movie && (
           <DetailSection>
             <img src={apiImage(movie.poster_path)} alt={movie.title} />
             <div>
-              <SectionTitle>
-                {movie.title}
-                &nbsp;
-                {movie.release_date && (
-                  <span>({movie.release_date.split('-')[0]})</span>
-                )}
-              </SectionTitle>
-              <p
-                css={{
-                  color: '#878787',
-                  fontStyle: 'italic',
-                  marginBottom: '32px',
-                  marginTop: '-16px',
-                }}
-              >
-                {movie.tagline}
-              </p>
-              <Rating rating={movie.vote_average} />
+              <StatsSection>
+                <div>
+                  <SectionTitle>
+                    {movie.title}
+                    &nbsp;
+                    {movie.release_date && (
+                      <span>({movie.release_date.split('-')[0]})</span>
+                    )}
+                  </SectionTitle>
+                  <p
+                    css={{
+                      color: '#878787',
+                      fontStyle: 'italic',
+                      marginTop: '-16px',
+                    }}
+                  >
+                    {movie.tagline}
+                  </p>
+                </div>
+                <div css={{ marginLeft: '32px' }}>
+                  <h3 css={{ marginBottom: '-8px' }}>User Score</h3>
+                  <Rating rating={movie.vote_average} />
+                </div>
+              </StatsSection>
               <h3>Overview</h3>
               <p>{movie.overview}</p>
             </div>
